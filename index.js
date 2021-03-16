3c512320592e2958a23033d031e90a3cac0a290b
chillerniko <13071601+chillerniko@users.noreply.github.com>
2021-03-16 12:14:37 -0500

    First commit notetaking-api

---
+++ index.js
@@ -0,0 +1,36 @@
+import express from 'express';
+import { graphqlHTTP } from 'express-graphql';
+import mongoose from 'mongoose';
+import schema from './schema';
+import cors from "cors";
+
+const app = express();
+const PORT = 4300;
+
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://localhost/notetaking_db", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.json({
+        message: 'Nodetaking API v1'
+    });
+});
+
+app.use(
+    '/graphql',
+    graphqlHTTP({
+        schema: schema,
+        graphiql: true
+    })
+);
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on PORT ${PORT}`);
+});
+
+
---
+++ resolvers.js
@@ -0,0 +1,24 @@
+import { updateArgument } from '@graphql-tools/utils';
+import Note from './models/note';
+
+export const resolvers = {
+    Query: {
+        async getNote(root, { _id }) {
+            return await Note.findById(_id)
+        },
+        async allNotes() {
+            return await Note.find();
+        }
+    },
+    Mutation: {
+        async createNote(root, { input }) {
+            return await Note.create(input);
+        },
+        async updateNote(root, { _id, input }) {
+            return await Note.findOneAndUpdate({ _id }, input, { new: true })
+        },
+        async deleteNote(root, {_id}) {
+            return await Note.findByIdAndRemove({_id})
+        }
+    }
+};
